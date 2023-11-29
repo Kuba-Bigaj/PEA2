638c73a8b6ae8f6e96ea10682f5217a6e6b8a180
Kuba-Bigaj <k.bigaj666@gmail.com>
2023-11-29 11:51:31 +0100

    Added old code to be repurposed

--- PEA2/PEA2.cpp
+++ PEA2/PEA2.cpp
@@ -1,6 +1,252 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <conio.h>
+#include <chrono>
 
-int main()
+struct strint
 {
-    std::cout << "Hello World!\n";
+	std::string str;
+	int num;
+
+	strint(std::string ins, int inn)
+	{
+		str = ins;
+		num = inn;
+	}
+
+	strint()
+	{
+		strint("", 0);
+	}
+};
+
+class App {
+	//data storage
+	int** matrix = nullptr;
+	int size = 0;
+
+	//static UI helper functions
+	static int create_sub_menu(std::string top_banner, std::string options[], std::string bot_banner, int number, int def_option) // displays a submenu with options, returns the number chosen
+	{
+		int chosen_option = def_option;
+		while (true)
+		{
+			system("cls");
+			std::cout << top_banner;
+			for (int i = 0; i < number; i++)
+			{
+				if (i == chosen_option)
+				{
+					std::cout << "==>";
+				}
+				std::cout << "\t" << options[i] << "\n";
+			}
+			std::cout << bot_banner;
+			switch (_getch())
+			{
+			case 72:
+				chosen_option--;
+				break;
+			case 80:
+				chosen_option++;
+				break;
+			case '\r':
+				return chosen_option;
+			default:
+				break;
+			}
+			chosen_option = (chosen_option < 0 ? 0 : (chosen_option >= number ? number - 1 : chosen_option));
+		}
+	}
+
+	static bool test_input_validity(std::string err_message)
+	{
+		if (std::cin.fail())
+		{
+			std::cout << err_message << "\n";
+			std::cin.clear();
+			std::cin.ignore(1000, '\n');
+			system("pause");
+			return false;
+		}
+		return true;
+	}
+
+	//data manipulation functions
+	void dealloc_matrix() //deallocates the matrix, sets size to 0
+	{
+		if (!matrix)
+		{
+			size = 0;
+			return;
+		}
+		for (int i = 0; i < size; i++)
+		{
+			delete matrix[i];
+		}
+		delete matrix;
+		matrix = nullptr;
+		size = 0;
+	}
+
+	void alloc_matrix(int s) //allocates the matrix
+	{
+		size = s;
+		matrix = new int*[s];
+		for (int i = 0; i < s; i++)
+		{
+			matrix[i] = new int[s];
+		}
+	}
+
+	void generate_random_data(int in_size) //allocates and fills the matrix with random numbers in range <1, 100>
+	{
+		dealloc_matrix();
+		alloc_matrix(in_size);
+
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				matrix[i][j] = i != j ? rand() % 100 + 1 : -1;
+			}
+		}
+	}
+
+	void show_data()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				std::cout << matrix[i][j] << "\t";
+			}
+			std::cout << "\n";
+		}
+		system("pause");
+	}
+
+	void read_data_from_file(std::string filename) //TODO adapt
+	{
+		std::ifstream file(filename);
+
+		if (!file.is_open())
+		{
+			std::cout << "File open error!\n";
+			system("pause");
+			return;
+		}
+
+		int f_size;
+		file >> f_size;
+
+		if (file.fail())
+		{
+			std::cout << "Size read error!\n";
+			file.close();
+			system("pause");
+			return;
+		}
+
+		dealloc_matrix();
+		alloc_matrix(f_size);
+
+		int val;
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				file >> val;
+				if (file.fail())
+				{
+					std::cout << "Data read error!\n";
+					break;
+				}
+				else
+					matrix[i][j] = val;
+			}
+		}
+		file.close();
+		return;
+	}
+
+	void load_data()
+	{
+		std::string filename;
+		std::cout << "Enter filename:\n";
+		std::cin >> filename;
+		if (test_input_validity("unsupported filename!\n"))
+		{
+			read_data_from_file(filename);
+			std::cout << "Loaded data: \n";
+			show_data();
+		}
+	}
+
+	//algorithms
+
+
+
+public:
+	void run()
+	{
+		std::string title = "MAIN MENU\n";
+		std::string credits = "Kuba Bigaj 2023\n";
+		std::string options[7] = { "Load data", "Generate data", "Show current data", "Brute force", "Branch and bound","Dynamic programming", "Exit" };
+		int chosen_option = 0;
+
+		while (true)
+		{
+			chosen_option = create_sub_menu(title, options, credits, 7, chosen_option);
+
+			switch (chosen_option)
+			{
+			case 0:
+				load_data();
+				break;
+			case 1:
+				break;
+			case 2:
+				show_data();
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				return;
+			default:
+				break;
+			}
+		}
+	}
+
+	void debug()
+	{
+	}
+
+	void run_tests()
+	{
+		
+	}
+};
+
+
+int main(int argc, char *argv[])
+{
+	App a;
+
+	if (argc > 1)
+	{
+		if (strcmp(argv[1], "-t") == 0)
+		{
+			a.run_tests();
+			return 0;
+		}
+	}
+	a.run();
+	return 0;
 }
